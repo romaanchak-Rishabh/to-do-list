521afaea2ed411f8212fe14fd8f3f40e4e33b4cf
Rishabh Tater <rishabhtater14@gmail.com>
2022-01-11 14:04:30 +0530

    Updates

---
+++ precache-manifest.6fdecd3456e194ab3805f1bf3da6a238.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "845b18b02699906f96e1",
+    "url": "./static/css/main.4f6be0d6.chunk.css"
+  },
+  {
+    "revision": "845b18b02699906f96e1",
+    "url": "./static/js/main.aee9eab0.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "a6b09332a4833ab670bb",
+    "url": "./static/css/2.a41151e4.chunk.css"
+  },
+  {
+    "revision": "a6b09332a4833ab670bb",
+    "url": "./static/js/2.32a4f1e7.chunk.js"
+  },
+  {
+    "revision": "49134449e668c5fb729fe077cd8cb8f0",
+    "url": "./index.html"
+  }
+];
